c4d99ee6463742dd887da9e58a55778413b613c6
AbayomiYuruba <abayomiartificial@gmail.com>
2025-10-26 16:33:58 -0500

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-1 import { defineConfig } from 'vite'
-    2 import react from '@vitejs/plugin-react'
+    1     import { defineConfig } from 'vite'
+    2     import react from '@vitejs/plugin-react'
     3
-    4 // https://vitejs.dev/config/
-    5 export default defineConfig({
-    6   plugins: [react()],
-    7   preview: {
-    8     host: '0.0.0.0',
-    9     port: 8080,
-   10     allowedHosts: [
-   11       'creametal.onrender.com',
-   12     ],
-   13   },
-   14 })
+    4     export default defineConfig({
+    5       plugins: [react()],
+    6       preview: {
+    7         host: '0.0.0.0',
+    8         port: 8080,
+    9         allowedHosts: [
+   10           'creametal.onrender.com',
+   11         ],
+   12       },
+   13     })
